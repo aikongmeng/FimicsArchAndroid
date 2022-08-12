37947504b021ecf8ed8c0913ca27645896685af2
aikongmeng <aikongmeng@gmail.com>
2022-08-12 10:56:44 +0800

    Update main.dart

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -4,7 +4,7 @@ import 'page/favorite_page.dart';
 import 'page/hi_native_page.dart';
 import 'page/recommend_page.dart';
 
-//至少要有主入口
+//至少要有一个主入口
 void main() => runApp(MyApp(FavoritePage()));
 //注册多个dart module
 @pragma(
